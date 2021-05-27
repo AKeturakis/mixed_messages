2c719b535cf48fd9ea25950ffda3b2b31774479c
AKeturakis <84077277+AKeturakis@users.noreply.github.com>
2021-05-27 12:30:24 +0300

    Fixed Math.random() bug

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,21 +1,21 @@
 let latinMottos = [
-    'Finis coronat opus', 
-    'Timendi causa est nescire', 
-    'Faber est suae quisque fortunae', 
-    'Aquila non capit muscas', 
-    'Non ducor duco', 
-    'Qui totum vult totum perdit', 
-    'Malum consilium quod mutari non potest', 
-    'Libertas perfundet omnia luce', 
-    'Amor vincit omnia', 
-    'Ars longa, vita brevis'];
+    '"Finis coronat opus"', 
+    '"Timendi causa est nescire"', 
+    '"Faber est suae quisque fortunae"', 
+    '"Aquila non capit muscas"', 
+    '"Non ducor duco"', 
+    '"Qui totum vult totum perdit"', 
+    '"Malum consilium quod mutari non potest"', 
+    '"Libertas perfundet omnia luce"', 
+    '"Amor vincit omnia"', 
+    '"Ars longa, vita brevis"'];
 let aperitifs = ['Negroni', 'Aperol Spritz', 'Campari', 'Autumn Leaves', 'Martini', 'Gin Tonic', 'Manhatan', 'Margarita', 'Mojito', 'Cosmopolitan'];
 let cuisines = ['French', 'Scandinavian', 'Middle East', 'Chinese', 'Japanese', 'Greek', 'Italian', 'Mediterranean', 'Indian', 'Near East'];
 const eveningRecommendation = () => {
-  let latinMotto = latinMottos[Math.floor(Math.random * 11)];
-  let aperitif = aperitifs[Math.floor(Math.random * 11)];
-  let cuisine = cuisines[Math.floor(Math.random * 11)];
+  let latinMotto = latinMottos[Math.floor(Math.random() * 11)];
+  let aperitif = aperitifs[Math.floor(Math.random() * 11)];
+  let cuisine = cuisines[Math.floor(Math.random() * 11)];
   return `Your Latin motto for this evening is ${latinMotto}. 
   You should contemplate it while enjoying a glass of ${aperitif} and exquisite ${cuisine} cuisine.`;
 };
-console.log(eveningRecommendation);
+console.log(eveningRecommendation());
